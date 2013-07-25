d24e86d153de19fe9f3cad0ba8ad7cbee6d8072d
Hifiboombox <zbradley96@gmail.com>
2013-07-25 01:42:39 -0700

    changed some stuff, not sure what

---
+++ cfg/extra/reference/remove_spaces.py
@@ -0,0 +1,11 @@
+with open("cvar_list_07-21-13_03-16.log","r") as r:
+	with open("cvar_list_without_spaces.txt","w") as w:
+		content = r.readlines()
+		for i in content:
+			w.write(i.split(":")[0] + '\n')
+
+	'''
+	for i in content:
+		i = i.split(':')[0]
+		f.write(i)
+	'''

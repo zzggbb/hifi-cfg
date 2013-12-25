fb559efdc0016f966e0621523ac6d58120d3d2ae
Hifiboombox <zbradley96@gmail.com>
2013-12-25 13:52:26 -0800

    updated tools

--- tools/cvars/cvars.py
+++ tools/cvars/cvars.py
@@ -1,3 +1,3 @@
 def cvars(cvarlog):
     with open(cvarlog,'r') as f:
-        return {a[0]: {x: y.replace('"','').split(', ')[1:] if x=='tags' else y for x,y in zip(['value','tags','desc'],a[1:])} for a in [map(str.strip, line.split(' :')) for line in f.readlines()[2:-2]]}
+        return {a[0]: {x: y.replace('"','').split(', ')[1:] if x=='tags' else y for x,y in zip(['value','tags','desc'],a[1:])} for a in [map(str.strip, b.split(' :')) for b in f.readlines()[2:-2]]}
